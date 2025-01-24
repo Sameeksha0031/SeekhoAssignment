24f4b4f947828d74aef3d0e6486f8e1a177e8dcb
sameeksha0031 <sameekshavinodiya@gmail.com>
2025-01-24 18:10:32 +0530

    Status bar color is added

--- app/src/main/java/com/example/seekhoassignment/HomeScreen.kt
+++ app/src/main/java/com/example/seekhoassignment/HomeScreen.kt
@@ -2,53 +2,96 @@ package com.example.seekhoassignment
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import androidx.navigation.NavController
 import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
+import com.example.seekhoassignment.model.Data
 import com.example.seekhoassignment.ui.theme.Blue100
 import com.example.seekhoassignment.ui.theme.Blue800
 import com.example.seekhoassignment.viewModel.AnimeViewModel
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.launch
 
 @Composable
-fun HomeScreen(mainActivity: MainActivity, navigationStack : NavController, apiViewModel: AnimeViewModel) {
-    val coroutineScope = rememberCoroutineScope()
-
-
-    //val animeListResponse = apiViewModel.animeList.
-
+fun HomeScreen(
+    mainActivity: MainActivity,
+    navigationStack: NavController,
+    apiViewModel: AnimeViewModel
+) {
+    val animeListResponse by apiViewModel.animeList.collectAsState()
 
     Box(
         modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight()
-    ) {
-        Column {
-            Box {
-            }
+            .fillMaxSize()
+            .statusBarsPadding()
+            .navigationBarsPadding()
+            .background(Blue100)
+    )
+    {
+        Column(
+            modifier = Modifier,
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.Start
+        ) {
+            Heading("Home Screen")
+            when (animeListResponse) {
+                is APIResult.Failure -> {
+                    Box {
+                        Text("No data found")
+                    }
+                }
 
-            LazyColumn {
-                items(10) {
-                    CardViewForList(mainActivity)
+                is APIResult.Loading -> {
+                    CircularProgressIndicator(
+                        modifier = Modifier
+                            .padding(1.dp)
+                            .width(20.dp)
+                            .height(20.dp),
+                        color = Color(0xFF888888),
+                        strokeWidth = 3.dp
+                    )
+                }
+
+                is APIResult.Success -> {
+                    val listOfTopAnime = (animeListResponse as APIResult.Success).response.data
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .fillMaxHeight()
+                    ) {
+                        Column {
+                            LazyColumn {
+                                items(listOfTopAnime.size) { index ->
+                                    CardViewForList(mainActivity, listOfTopAnime[index])
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
@@ -57,52 +100,75 @@ fun HomeScreen(mainActivity: MainActivity, navigationStack : NavController, apiV
 
 @SuppressLint("CheckResult")
 @Composable
-fun CardViewForList(mainActivity: MainActivity) {
+fun CardViewForList(mainActivity: MainActivity, listOfTopAnime: Data) {
     Card(
         modifier = Modifier
             .padding(8.dp)
-            .fillMaxWidth()
-            .background(Blue800),
+            .fillMaxWidth(),
         shape = RoundedCornerShape(12),
+        colors = CardColors(
+            containerColor = Blue800,
+            contentColor = Blue800,
+            disabledContentColor = Blue100,
+            disabledContainerColor = Blue100
+        )
     ) {
         Column(
             modifier = Modifier
                 .padding(8.dp)
         ) {
             Text(
-                "Title", style = TextStyle(
+                text = listOfTopAnime.title, style = TextStyle(
                     fontSize = 14.sp,
                     fontWeight = FontWeight.Bold,
                     color = Blue100,
                 ), modifier = Modifier.padding(3.dp)
             )
             Text(
-                "Number Of Episodes", style = TextStyle(
+                "Number Of Episodes: ${listOfTopAnime.episodes}", style = TextStyle(
                     fontSize = 14.sp,
                     fontWeight = FontWeight.Bold,
                     color = Blue100,
                 ), modifier = Modifier.padding(3.dp)
             )
             Text(
-                "Rating", style = TextStyle(
+                "Rating: ${listOfTopAnime.rating}", style = TextStyle(
                     fontSize = 14.sp,
                     fontWeight = FontWeight.Bold,
                     color = Blue100,
                 ), modifier = Modifier.padding(3.dp)
             )
             Glide.with(mainActivity)
-                .load("https://picsum.photos/200/300")
+                .load(listOfTopAnime.url)
         }
     }
 }
 
 @Composable
-fun TextWithStyle(stringToShow : String, isTitle : Boolean) {
+fun TextWithStyle(stringToShow: String, isTitle: Boolean) {
     Text(
         text = stringToShow, style = TextStyle(
             fontSize = 14.sp,
-            fontWeight = if(isTitle) FontWeight.Bold else FontWeight(300),
+            fontWeight = if (isTitle) FontWeight.Bold else FontWeight(300),
             color = Blue100,
         ), modifier = Modifier.padding(3.dp)
+    )
+}
+
+@Composable
+fun Heading(stringToShow: String) {
+    Text(
+        text = stringToShow,
+        style = TextStyle(
+            fontSize = 20.sp,
+            fontWeight = FontWeight(700),
+            color = Blue100,
+        ),
+        modifier = Modifier
+            .height(60.dp)
+            .padding()
+            .fillMaxWidth()
+            .background(Blue800)
+            .padding(top = 20.dp, start = 10.dp),
     )
 }
--- app/src/main/java/com/example/seekhoassignment/MainActivity.kt
+++ app/src/main/java/com/example/seekhoassignment/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.seekhoassignment
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,7 +12,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.core.view.WindowCompat
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.seekhoassignment.ui.theme.Blue800
 import com.example.seekhoassignment.ui.theme.SeekhoAssignmentTheme
 import com.example.seekhoassignment.viewModel.AnimeViewModel
 
@@ -21,12 +25,21 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            changeStatusBarColor(this)
             SeekhoAssignmentTheme {
                 val apiViewModel = viewModel { AnimeViewModel() }
+                apiViewModel.getList()
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     NavigationStack(apiViewModel,this@MainActivity)
                 }
             }
+        }
+    }
+
+    private fun changeStatusBarColor(activity: Activity) {
+        activity.window.statusBarColor = resources.getColor(R.color.blue800)
+        WindowCompat.getInsetsController(activity.window, activity.window.decorView).apply {
+            isAppearanceLightStatusBars = false
         }
     }
 }
